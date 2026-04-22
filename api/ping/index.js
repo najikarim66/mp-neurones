e8aed4dfeb8cbabc4ce479cfabb70382900b25e8
najikarim66 <naji@neurones.ma>
2026-04-22 10:10:29 +0100

    Delete api/ping/index.js

--- api/ping/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (context, req) {
-  context.res = {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-    body: { ok: true, now: new Date().toISOString(), hasCosmosEnv: !!process.env.COSMOS_CONNECTION_STRING }
-  };
-};
